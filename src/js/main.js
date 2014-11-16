93da134d27e5fc8b94d786b7002f1b65a6c8796d
Souvik Basu <souvikbasu@gmail.com>
2014-11-16 12:58:24 +0530

    added fnc main object

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 require(['ui_controls/globals/rootVisual', 'ui_controls/panels/grid'], function(rootVisual, grid){
-    body = new rootVisual(new grid());
+    fnc.body = new rootVisual(new grid());
+    if(fnc.main) {
+        fnc.main();
+    }
 });
