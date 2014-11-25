2cf33cfb68236f69a371f1f63de1b4ea86f5eca5
Souvik Basu <souvikbasu@gmail.com>
2014-11-25 13:31:34 +0530

    Set height and width of stackpanel as per its children

--- src/js/ui_controls/panels/stackpanel.js
+++ src/js/ui_controls/panels/stackpanel.js
@@ -9,18 +9,34 @@ define(function (require) {
         var orientation = this.properties["orientation"] || 'horizontal';
         if(orientation === 'horizontal') {
             var currentLeft = 0;
+            var maxHeight = 0;
             for(var i= 0, child; child=this.children.get(i); i++) {
                 var style = child.dom.style;
                 style.left = currentLeft + 'px';
-                currentLeft = currentLeft + parseInt(style.width.slice(0,-2));
+                var width = parseInt(style.width.slice(0, -2));
+                var height = parseInt(style.height.slice(0, -2));
+                currentLeft = currentLeft + width;
+                if(height > maxHeight) {
+                    maxHeight = height;
+                }
             }
+            this.dom.style.width = currentLeft + 'px';
+            this.dom.style.height = maxHeight + 'px';
         } else {
             var currentTop = 0;
+            var maxWidth = 0;
             for(var i= 0, child; child=this.children.get(i); i++) {
                 var style = child.dom.style;
                 style.top = currentTop + 'px';
-                currentTop = currentTop + parseInt(style.height.slice(0,-2));
+                var height = parseInt(style.height.slice(0, -2));
+                var width = parseInt(style.width.slice(0, -2));
+                currentTop = currentTop + height;
+                if(width > maxWidth) {
+                    maxWidth = width;
+                }
             }
+            this.dom.style.height = currentTop + 'px';
+            this.dom.style.width = maxWidth + 'px';
         }
     }
 
