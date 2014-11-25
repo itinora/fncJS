2fe2a39e7b5dfd612437fc355b6a7e0e7d3d2ef3
Souvik Basu <souvikbasu@gmail.com>
2014-11-25 10:34:45 +0530

    Added support for grid.rowspan and grid.colspan in uiElement

--- src/js/ui_controls/ui_element.js
+++ src/js/ui_controls/ui_element.js
@@ -55,11 +55,41 @@ define(function (require) {
                 if(this.dom.tagName !== 'SELECT' && this.dom.getAttribute('type') !== 'radio') { //for radio buttons and select controls if height and width are set it shows up big in size
                     style.height = grid.rows[rowIndex].height + "px";
                 }
+            } else if(key === "grid.rowspan") {
+                var rowSpan = parseInt(this.properties[key]);
+                var grid = this["grid"];
+                var startWithRow = 0;
+                var gridRowProperty = this.properties["grid.row"];
+                if(gridRowProperty) {
+                    startWithRow = gridRowProperty;
+                }
+                var height = 0;
+                for(var r=0, row; row = grid.rows[startWithRow + r], r < rowSpan; r++) {
+                        height = height + row["width"];
+                }
+
+                if(this.dom.tagName !== 'SELECT' && this.dom.getAttribute('type') !== 'radio') { //for radio buttons and select controls if height and width are set it shows up big in size
+                    style.height = height + "px";
+                }
             } else if(key === "grid.col") {
                 var colIndex = parseInt(this.properties[key]);
                 var grid = this["grid"];
                 style.left = grid.cols[colIndex].left + "px";
                 style.width = grid.cols[colIndex].width + "px";
+            } else if(key === "grid.colspan") {
+                var colSpan = parseInt(this.properties[key]);
+                var grid = this["grid"];
+                var startWithCol = 0;
+                var gridColProperty = this.properties["grid.col"];
+                if(gridColProperty) {
+                    startWithCol = gridColProperty;
+                }
+                var width = 0;
+                for(var c=0, col; col = grid.cols[startWithCol + c], c < colSpan; c++) {
+                        width = width + col["width"];
+                }
+
+                style.width = width + "px";
             }
         }
     };
